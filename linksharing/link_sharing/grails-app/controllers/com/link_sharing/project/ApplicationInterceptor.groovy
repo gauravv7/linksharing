b65eccca790132dd1b3621e993b4d35c300f4a4c
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-07 17:41:01 +0530

    question 12: added session filter logging for all controllers

--- linksharing/link_sharing/grails-app/controllers/com/link_sharing/project/ApplicationInterceptor.groovy
+++ linksharing/link_sharing/grails-app/controllers/com/link_sharing/project/ApplicationInterceptor.groovy
@@ -8,6 +8,14 @@ class ApplicationInterceptor {
     }
 
     boolean before() {
+
+        if(session.user==null) {
+            log.info("guest user")
+        } else if (session.user.admin==true) {
+            log.info("admin user")
+        } else if(session.user.admin == false) {
+            log.info("normal user")
+        }
         log.info "controller: $controllerName, action:$actionName -> $params"
         true
     }
