0a6f234c57303e2e505010baeb218a6f3a5e2823
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-07 17:23:28 +0530

    question2-10: loginHandler, logout functionality added with test cases

--- linksharing/link_sharing/grails-app/controllers/com/link_sharing/project/LoginController.groovy
+++ linksharing/link_sharing/grails-app/controllers/com/link_sharing/project/LoginController.groovy
@@ -2,9 +2,31 @@ package com.link_sharing.project
 
 class LoginController {
 
-    def index() { }
+    def index() {
+        if (session?.user) {
+            forward(controller: 'user', action: 'index')
+        } else {
+            response.sendError(404)
+        }
+    }
 
-    def loginHandler() {}
+    def loginHandler(String userName, String password) {
+        User user = User.findByUserNameAndPassword(userName, password)
 
-    def logout() {}
+        if (user) {
+            if (user.active) {
+                session.user = user
+                redirect(controller: 'login', action: 'index')
+            } else {
+                render flash.error = "User is not active"
+            }
+        } else {
+            render flash.error = "User not found"
+        }
+    }
+
+    def logout() {
+        session.invalidate()
+        redirect(action: 'index')
+    }
 }
