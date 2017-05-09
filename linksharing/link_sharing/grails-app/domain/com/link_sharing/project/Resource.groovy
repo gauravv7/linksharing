661e6eada68e4dfcbd2cf863662e30158f351321
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-09 15:47:48 +0530

    quesion 5: added getTopPosts()

--- linksharing/link_sharing/grails-app/domain/com/link_sharing/project/Resource.groovy
+++ linksharing/link_sharing/grails-app/domain/com/link_sharing/project/Resource.groovy
@@ -5,6 +5,7 @@ import com.link_sharing.project.Topic as Topic
 import com.link_sharing.project.ResourceRating as ResourceRating
 import com.link_sharing.project.ReadingItem as ReadingItem
 import com.link_sharing.project.co.ResourceSearchCO
+import com.link_sharing.project.vo.PostVO
 import com.link_sharing.project.vo.RatingInfoVO
 
 abstract class Resource {
@@ -60,6 +61,43 @@ abstract class Resource {
             order('totalVotes', 'desc')
         }
         new RatingInfoVO(totalVotes: result[0], averageScore: result[1], totalScore: result[2])
+    }
+
+    static List<PostVO> getTopPosts() {
+        List<PostVO> topPosts = []
+        ResourceRating.createCriteria().list(max: 5) {
+            projections {
+                property('resource.id')
+
+                'resource'{
+                    property('description')
+                    property('url')
+                    property('filePath')
+                    'topic' {
+                        property('id')
+                        property('topicName')
+                        eq('visibility', enums.Visibility.PUBLIC)
+                    }
+                    'createdBy' {
+                        property('id')
+                        property('userName')
+                        property('firstName')
+                        property('lastName')
+                        property('photo')
+                    }
+                    property('lastUpdated')
+                }
+            }
+            groupProperty('resource.id')
+            count('id', 'totalVotes')
+            order('totalVotes', 'desc')
+        }?.each {
+            topPosts.add(new PostVO(resourceID: it[0], description: it[1], url: it[2], filePath: it[3], topicID:
+                    it[4], topicName: it[5], userID: it[6], userName: it[7], userFirstName: it[8], userLastName: it[9],
+                    userPhoto: it[10], isRead: "", resourceRating: 0, postDate: it[11]))
+        }
+
+        return topPosts
     }
 
     String toString() {
